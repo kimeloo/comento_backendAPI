2291887b7a0541232a28c1e9ea88969af69f49a1
kimeloo <iroo200121@gmail.com>
2024-07-26 14:41:02 +0900

    FIX : loginCount API dto 추가

--- comentoStatistic/src/main/java/com/demo/comentoStatistic/dao/StatisticMapper.java
+++ comentoStatistic/src/main/java/com/demo/comentoStatistic/dao/StatisticMapper.java
@@ -1,6 +1,10 @@
 package com.demo.comentoStatistic.dao;
 
 
+import com.demo.comentoStatistic.dto.YearCountDto;
+import com.demo.comentoStatistic.dto.YearMonthCountDto;
+import org.apache.ibatis.annotations.Mapper;
+
 @Mapper
 public interface StatisticMapper {
 
